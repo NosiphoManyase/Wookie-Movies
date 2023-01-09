3d15c5caf6155dac499940b801047105ea7b093c
Nosipho <manyasenosipho@gmail.com>
2023-01-09 09:21:20 +0200

    configure page link to single movie

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Movie = () => {
 
+  const location = useLocation()
+  const movie  = location.state
+  console.log(movie)
+
+  const year = movie.released_on.split('-')[0]
+  const director = Array.isArray(movie.director)?movie.director.join(', '):movie.director
+  const cast = movie.cast.join(', ')
+
   return (
-    <div>
-      Movie
+    <div style={{display:'flex'}}>
+      <div>
+        <img  src={movie.poster} style={{width: '300px'}}/>
+      </div>
+      <div>
+        <div style={{display:'flex'}}>
+          <p>{movie.title}<span>({movie.imdb_rating})</span></p>
+          <p>star rating</p>
+        </div>
+        <div>
+          {year} | {movie.length} | {director}
+          <p>cast: {cast}</p>
+        </div>
+        <div>
+          <p>Movie Description: {movie.overview}</p>
+        </div>
+      </div>
     </div>
   )
 }
